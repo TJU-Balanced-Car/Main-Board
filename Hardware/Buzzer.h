9277fda57d1dc2638b8c8c0e28e952a552276219
mdfucker <9263313@qq.com>
2024-09-26 23:18:23 +0800

    蜂鸣器初始化

---
+++ Hardware/Buzzer.h
@@ -0,0 +1,16 @@
+/*
+ * Buzzer.h
+ *
+ *  Created on: 2024Äê9ÔÂ26ÈÕ
+ *      Author: mdfucker
+ */
+
+#ifndef HARDWARE_BUZZER_H_
+#define HARDWARE_BUZZER_H_
+
+void Buzzer_Init(void);
+void Buzzer_Ring(void);
+void Buzzer_Stop(void);
+
+
+#endif /* HARDWARE_BUZZER_H_ */
